b43f800969baa99c99b68430833c21732d3dbc1b
mtibaldi486 <tibaldi.math@gmail.com>
2020-01-23 01:09:29 +0100

    script 2

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 sudo apt install mysql-server
-
+sudo apt install make
+sudo apt install gcc
+sudo apt install libmysqlclient-dev
+sudo apt install libgtk-3-dev
+sudo apt install libssl-dev
+sudo apt install libcurl-dev
